d37d0994b72c2e08ed77e90ac189c9cce1608974
Martin <mrt.thonon@gmail.com>
2024-01-09 23:53:50 +0100

    REFACTOR/UPDATE/CREATE file

--- src/map/map_parse.c
+++ src/map/map_parse.c
@@ -8,9 +8,8 @@
 
 static uint8_t			ft_check_extension(char *str, char *ext);
 static uint8_t			ft_open_file(char *file, int32_t *fd);
-static t_map_exception	ft_check_requirement(int32_t fd, t_map *map);
-static t_map_exception	ft_check_map(int32_t fd, t_map *map);
-//static t_map_exception	ft_read_file(int32_t fd);
+static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i);
+static t_map_exception	ft_check_map( t_map *map, int32_t fd);
 
 char *ft_read_line(char **str, int32_t fd);
 
@@ -23,12 +22,12 @@ t_map_exception ft_parse_map(char *file, t_map *map)
 		return (EXTENSION_ERROR);
 	if (1 == ft_open_file(file, &fd))
 		return (OPEN_ERROR);
-	map_exception = ft_check_requirement(fd, map);
+	map_exception = ft_check_requirement(map, fd, 0);
 	if (map_exception >= REQUIREMENT_ERROR && map_exception <= RGB_ERROR) {
 		printf("here\n");
 		return (close(fd), map_exception);
 	}
-	if (ELEMENT_ERROR == ft_check_map(fd, map))
+	if (ELEMENT_ERROR == ft_check_map(map, fd))
 		return (ELEMENT_ERROR);
 	/* set map */
 	close(fd);
@@ -51,39 +50,30 @@ uint8_t ft_open_file(char *file, int32_t *fd)
 	return (-1 == *fd);
 }
 
-static t_map_exception	ft_check_requirement(int32_t fd, t_map *map)
+static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i)
 {
-	const t_dictionary dictionary[N_REQUIREMENT] = {
-			{REQUIREMENT_NORTH_TEXTURE, 2},
-			{REQUIREMENT_SOUTH_TEXTURE, 2},
-			{REQUIREMENT_WEST_TEXTURE, 2},
-			{REQUIREMENT_EAST_TEXTURE, 2},
-			{REQUIREMENT_FLOOR_RGB, 1},
-			{REQUIREMENT_CEILING_RGB, 1},
+	static const char dictionary[N_REQUIREMENT][REQUIREMENT_LEN] = {
+			REQUIREMENT_NORTH_TEXTURE,REQUIREMENT_SOUTH_TEXTURE,
+			REQUIREMENT_WEST_TEXTURE,REQUIREMENT_EAST_TEXTURE,
+			REQUIREMENT_FLOOR_RGB,REQUIREMENT_CEILING_RGB
 	};
-	size_t i;
 	char **requirement;
 	char *line;
 
-	i = 0;
-	while (ft_read_line(&line, fd) && i < 6)
-	{
-		requirement = ft_split(line, ' ');
-		if (requirement == NULL || requirement[1] == NULL)
-			return (REQUIREMENT_ERROR);
-		printf("%s.\n", requirement[0]);
-		if (ft_strncmp(dictionary[i].requirement, requirement[0], dictionary[i].len) != 0)
-		{
-			printf("%s, %s\n", dictionary[i].requirement, requirement[0]);
-			return (REQUIREMENT_ERROR);
-		}
-		++i;
-	}
-	(void)map;
+	ft_read_line(&line, fd);
+	requirement = ft_split(line, ' ');
+	if (requirement == NULL || requirement[1] == NULL || ft_strcmp(dictionary[i], requirement[0]) != 0)
+		return (ft_freef("%p, %P", line, requirement), REQUIREMENT_ERROR);
+	if (ft_check_path(requirement[i], map) == 0)
+		;
+	ft_freef("%p, %P", line, requirement);
+	if (i < N_REQUIREMENT && ft_check_requirement(map, fd, ++i) == 3)
+		return (REQUIREMENT_ERROR);
 	return (NO_MAP_EXCEPTION);
 }
 
-static t_map_exception	ft_check_map(int32_t fd, t_map *map)
+
+static t_map_exception	ft_check_map( t_map *map, int32_t fd)
 {
 	(void)fd;
 	(void)map;
