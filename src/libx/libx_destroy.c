a89226fa3dfe0596ecab3b050d5dfbbdbb611163
Simon Hinfray <108767313+shinfray@users.noreply.github.com>
2024-01-12 18:13:36 +0100

    FIX linux compilation

--- src/libx/libx_destroy.c
+++ src/libx/libx_destroy.c
@@ -3,6 +3,8 @@
 
 void	ft_destroy_graphx(t_graphx *graphx);
 
+#ifdef __APPLE__
+
 void	ft_destroy_graphx(t_graphx *graphx)
 {
 	//mlx_loop_end(graphx->mlx_ptr);
@@ -12,3 +14,17 @@ void	ft_destroy_graphx(t_graphx *graphx)
 	//mlx_destroy_display(graphx->mlx_ptr);
 	free(graphx->mlx_ptr);
 }
+
+#elif __linux__
+
+void	ft_destroy_graphx(t_graphx *graphx)
+{
+	mlx_loop_end(graphx->mlx_ptr);
+	/* could be a loop if minimap */
+	mlx_destroy_image(graphx->mlx_ptr, graphx->window.image.mlx_img);
+	mlx_destroy_window(graphx->mlx_ptr, graphx->window.mlx_win);
+	mlx_destroy_display(graphx->mlx_ptr);
+	// free(graphx->mlx_ptr);
+}
+
+#endif
--- src/libx/libx_init.c
+++ src/libx/libx_init.c
@@ -6,6 +6,8 @@ static void		*ft_mlx_new_window(void *mlx_ptr, void **mlx_win);
 static void		*ft_mlx_new_image(void *mlx_ptr, void **mlx_img);
 static void		ft_mlx_get_data_addr(t_image *image);
 
+#ifdef __APPLE__
+
 t_mlx_exception	ft_init_graphx(t_game_data *game_data, t_graphx *graphx)
 {
 	if (NULL == ft_mlx_init(&graphx->mlx_ptr))
@@ -29,6 +31,34 @@ t_mlx_exception	ft_init_graphx(t_game_data *game_data, t_graphx *graphx)
 	mlx_key_hook(graphx->window.mlx_win, &ft_key_up_handler, game_data);
 	return (NO_EXCEPTION);
 }
+
+#elif __linux__
+
+t_mlx_exception	ft_init_graphx(t_game_data *game_data, t_graphx *graphx)
+{
+	if (NULL == ft_mlx_init(&graphx->mlx_ptr))
+		return (INIT_ERROR);
+	if (NULL == ft_mlx_new_window(graphx->mlx_ptr, &graphx->window.mlx_win))
+	{
+		mlx_destroy_display(graphx->mlx_ptr);
+		return (CREATE_WINDOW_ERROR);
+	}
+	if (NULL == ft_mlx_new_image(graphx->mlx_ptr, &graphx->window.image.mlx_img)) /* need loop if add minimap */
+	{
+		mlx_destroy_window(graphx->mlx_ptr, graphx->window.mlx_win);
+		mlx_destroy_display(graphx->mlx_ptr);
+		return (CREATE_IMAGE_ERROR);
+	}
+	ft_mlx_get_data_addr(&graphx->window.image);
+	mlx_hook(graphx->window.mlx_win, ON_DESTROY, 0, &ft_on_destroy, game_data);
+	mlx_hook(graphx->window.mlx_win, ON_KEYDOWN, (1L << 0), &ft_key_handler, game_data);
+	graphx->mouse_tracked = false;
+	mlx_hook(graphx->window.mlx_win, ON_MOUSEMOVE, 0, &ft_on_mouse_move, game_data);
+	mlx_key_hook(graphx->window.mlx_win, &ft_key_up_handler, game_data);
+	return (NO_EXCEPTION);
+}
+
+#endif
 
 static void	*ft_mlx_init(void **mlx_ptr)
 {
