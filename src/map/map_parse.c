94bf4fdbef6344b8f911eea23ebaa0f2c6b080cf
martinthonon <mrt.thonon@gmail.com>
2024-01-16 16:42:44 +0100

    REFACTOR file

--- src/map/map_parse.c
+++ src/map/map_parse.c
@@ -6,12 +6,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct s_xy {size_t y; size_t x; size_t tmp;} t_xy;
-
 static uint8_t			ft_check_extension(char *str, char *ext);
 static uint8_t			ft_open_file(char *file, int32_t *fd);
 static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i);
-static t_map_exception	ft_check_map( t_map *map, int32_t fd, size_t y, size_t x);
+static t_map_exception	ft_check_map( t_map *map, int32_t fd);
 
 uint8_t ft_set_path(t_map *map, t_dictionary lexic, char *args);
 uint8_t ft_set_rgb(t_map *map, t_dictionary lexic, char *args);
@@ -32,11 +30,11 @@ t_map_exception ft_parse_map(char *file, t_map *map)
 	map_exception = ft_check_requirement(map, fd, 0);
 	if (map_exception >= REQUIREMENT_ERROR && map_exception <= RGB_ERROR)
 		return (close(fd), map_exception);
-	if (ELEMENT_ERROR == ft_check_map(map, fd, 0, 0))
-		return (ELEMENT_ERROR);
-	/* set map */
+	map->grid = NULL;
+	if (ELEMENT_ERROR == ft_check_map(map, fd))
+		return (close(fd), ELEMENT_ERROR);
 	close(fd);
-	return (map_exception);
+	return (NO_MAP_EXCEPTION);
 }
 
 uint8_t ft_check_extension(char *str, char *ext)
@@ -70,7 +68,7 @@ static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i)
 
 	ft_read_line(&line, fd);
 	args = ft_split(line, ' ');
-	if (ft_dptrlen((const char **)args) != 2 || ft_strcmp(lexic[i].rule, args[0]) != 0)
+	if (ft_dptrlen((const char **)args) != 2 || ft_strcmp(lexic[i].rule, args[0]) != 0) /* modify for cp and fp */
 		return (ft_freef("%p, %P", line, args), REQUIREMENT_ERROR);
 	if (1 == ft_set_args(map, lexic[i], args[1]))
 		return (ft_freef("%p, %P", line, args), lexic[i].exception);
@@ -81,19 +79,33 @@ static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i)
 }
 
 
-static t_map_exception	ft_check_map(t_map *map, int32_t fd, size_t y, size_t x)
+static t_map_exception	ft_check_map(t_map *map, int32_t fd)
 {
-	size_t	tmp;
-	char	*line;
+	t_element	*tmp;
+	size_t		i;
+	char		*line;
 
 	if (NULL == ft_read_line(&line, fd))
 		return (NO_MAP_EXCEPTION);
-	if (ELEMENT_ERROR == ft_check_line())
-		return (ELEMENT_ERROR);
-	if (tmp > x)
-		x = tmp;
-	/* ft_join */
-	return (ft_check_map(map, fd, ++y, x));
+	i = 0;
+	while (line[i] != '\0')
+	{
+		if ('\n' == line[i])
+		{
+			line[i] = '\0';
+			break;
+		}
+		if (' ' == line[i])
+			line[i] = '0';
+		printf("%c\n", line[i]);
+		if (line[i] < '0' || line[i] > '1')
+			return (ELEMENT_ERROR);
+		++i;
+	}
+	tmp = map->grid;
+	ft_join(2, map->grid, line);
+	free (tmp);
+	return (ft_check_map(map, fd));
 }
 
 char *ft_read_line(char **str, int32_t fd)
@@ -119,7 +131,8 @@ uint8_t ft_set_path(t_map *map, t_dictionary lexic, char *args)
 	const size_t len = ft_strlen(args);
 	int32_t fd;
 
-	args[len - 1] = '\0';
+	if  ('\n' == args[len - 1])
+		args[len - 1] = '\0';
 	if (len >= PATH_MAX || 1 == ft_open_file(args, &fd))
 		return (1);
 	close(fd);
