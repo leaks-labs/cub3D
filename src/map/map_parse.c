b43f7aefc61c5337bdaad3aab9518e9bc931b797
martinthonon <mrt.thonon@gmail.com>
2024-01-17 17:30:34 +0100

    REFACTOR file

--- src/map/map_parse.c
+++ src/map/map_parse.c
@@ -9,7 +9,7 @@
 static uint8_t			ft_check_extension(char *str, char *ext);
 static uint8_t			ft_open_file(char *file, int32_t *fd);
 static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i);
-static t_map_exception	ft_check_map( t_map *map, int32_t fd);
+static t_map_exception	ft_check_map( t_map *map, char *tmp_map, int32_t fd);
 
 uint8_t ft_set_path(t_map *map, t_dictionary lexic, char *args);
 uint8_t ft_set_rgb(t_map *map, t_dictionary lexic, char *args);
@@ -21,6 +21,7 @@ char *ft_read_line(char **str, int32_t fd);
 t_map_exception ft_parse_map(char *file, t_map *map)
 {
 	t_map_exception	map_exception;
+	char *tmp_map;
 	int32_t			fd;
 
 	if (1 == ft_check_extension(file, (char *)".pub"))
@@ -30,8 +31,8 @@ t_map_exception ft_parse_map(char *file, t_map *map)
 	map_exception = ft_check_requirement(map, fd, 0);
 	if (map_exception >= REQUIREMENT_ERROR && map_exception <= RGB_ERROR)
 		return (close(fd), map_exception);
-	map->grid = NULL;
-	if (ELEMENT_ERROR == ft_check_map(map, fd))
+	tmp_map = NULL;
+	if (ELEMENT_ERROR == ft_check_map(map, tmp_map, fd))
 		return (close(fd), ELEMENT_ERROR);
 	close(fd);
 	return (NO_MAP_EXCEPTION);
@@ -65,10 +66,10 @@ static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i)
 	};
 	char **args;
 	char *line;
-
+	/*indexadr*/
 	ft_read_line(&line, fd);
 	args = ft_split(line, ' ');
-	if (ft_dptrlen((const char **)args) != 2 || ft_strcmp(lexic[i].rule, args[0]) != 0) /* modify for cp and fp */
+	if (ft_dptrlen((const char **)args) != 2 || ft_strcmp(lexic[i].rule, args[0]) != 0) /* pattern matching to return an index */
 		return (ft_freef("%p, %P", line, args), REQUIREMENT_ERROR);
 	if (1 == ft_set_args(map, lexic[i], args[1]))
 		return (ft_freef("%p, %P", line, args), lexic[i].exception);
@@ -79,33 +80,27 @@ static t_map_exception	ft_check_requirement(t_map *map, int32_t fd, size_t i)
 }
 
 
-static t_map_exception	ft_check_map(t_map *map, int32_t fd)
+static t_map_exception	ft_check_map(t_map *map, char *tmp_map, int32_t fd)
 {
-	t_element	*tmp;
-	size_t		i;
-	char		*line;
+	size_t	i;
+	char	*line;
 
 	if (NULL == ft_read_line(&line, fd))
 		return (NO_MAP_EXCEPTION);
 	i = 0;
 	while (line[i] != '\0')
 	{
-		if ('\n' == line[i])
-		{
-			line[i] = '\0';
-			break;
-		}
-		if (' ' == line[i])
+		if (ft_isspace(line[i]))
 			line[i] = '0';
 		printf("%c\n", line[i]);
 		if (line[i] < '0' || line[i] > '1')
 			return (ELEMENT_ERROR);
 		++i;
 	}
-	tmp = map->grid;
+	tmp_map = map->grid;
 	ft_join(2, map->grid, line);
 	free (tmp);
-	return (ft_check_map(map, fd));
+	return (ft_check_map(map, tmp_map, fd));
 }
 
 char *ft_read_line(char **str, int32_t fd)
