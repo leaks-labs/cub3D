3720598b41f5627228ecba4fc8c700310ff2e53a
Simon Hinfray <108767313+shinfray@users.noreply.github.com>
2024-01-19 11:52:35 +0100

    ADD structurenotifymask for linux

--- src/libx/libx_init.c
+++ src/libx/libx_init.c
@@ -80,12 +80,13 @@ static void	*ft_mlx_new_window(void *mlx_ptr, void **mlx_win)
 
 static void	ft_set_hooks(t_game *game, t_graphx *graphx)
 {
-	mlx_hook(graphx->s_window.mlx_win, ON_DESTROY, 0, &ft_on_destroy, game);
 	mlx_hook(graphx->s_window.mlx_win, ON_KEYDOWN, (1L << 0), \
 				&ft_key_down_handler, game);
 	mlx_hook(graphx->s_window.mlx_win, ON_KEYUP, (1L << 1), \
 				&ft_key_up_handler, game);
 	mlx_hook(graphx->s_window.mlx_win, ON_MOUSEMOVE, (1L << 6), \
 				&ft_on_mouse_move, game);
+	mlx_hook(graphx->s_window.mlx_win, ON_DESTROY, (1L << 17), \
+				&ft_on_destroy, game);
 	mlx_loop_hook(graphx->mlx_ptr, &ft_render, game);
 }
