ba8cb5a5ad052d0343314005747c0ad829dd4a90
Martin <mrt.thonon@gmail.com>
2024-02-01 19:20:57 +0100

    REFACTOR/UPDATE file

--- src/game/game_destroy.c
+++ src/game/game_destroy.c
@@ -5,7 +5,7 @@ void	ft_destroy_game(t_game *game);
 void	ft_destroy_game(t_game *game)
 {
 	ft_destroy_map(game->map);
+	ft_destroy_texture(game->graphx->mlx_ptr, game->map->texture, \
+                            N_ORIENTATION, N_ELEMENT);
 	ft_destroy_graphx(game);
-	// destroy the texture catalogue
-	/* Then destroy the rest of the struct */
 }
--- src/game/run.c
+++ src/game/run.c
@@ -1,6 +1,12 @@
 #include "game.h"
 
-uint8_t	ft_run(t_map *map);
+uint8_t			ft_run(t_map *map);
+t_mlx_exception	ft_open_texture(t_texture *texture, void *mlx_ptr);
+void			ft_destroy_texture(void *mlx_ptr, t_texture *texture, \
+									t_orientation e_orientation, \
+									t_element e_element);
+
+static int		ft_get_xpm(void *mlx_ptr, t_image *img, char *path);
 
 uint8_t	ft_run(t_map *map)
 {
@@ -13,66 +19,82 @@ uint8_t	ft_run(t_map *map)
 		ft_destroy_map(s_game.map);
 		return (1);
 	}
-
-	t_element		element;
-	t_orientation	orientation;
-	t_image			image
-
-	element = WALL;
-	while (element < N_ELEMENT)
+	if (ft_throw_mlx_exception(ft_open_texture(map->texture, s_graphx.mlx_ptr)))
 	{
-		orientation = NORTH;
-		while (orientation < N_ORIENTATION)
-		{
-			map->texture[element].path[N_ORIENTATION]
-			++orientation;
-		}
-		++element;
+		ft_destroy_map(s_game.map);
+		ft_destroy_graphx(&s_game);
+		return (1);
 	}
-	ft_destroy_game(&s_game);
 	mlx_loop(s_graphx.mlx_ptr);
 	return (0);
 }
 
-/* to remove (just for testing)*/
-//	const char	*f1 = "./maps/textures/purplestone.xpm";
-//	const char	*f2 = "./maps/textures/wood.xpm";
-//	const char	*f3 = "./maps/textures/mossy.xpm";
-//	const char	*f4 = "./maps/textures/greystone.xpm";
-//	const char	*f5 = "./maps/textures/colorstone.xpm";
-//	const char	*f6 = "./maps/textures/bluestone.xpm";
-//	t_image		*i1 = &s_game.map->texture[WALL].image[NORTH];
-//	t_image		*i2 = &s_game.map->texture[WALL].image[SOUTH];
-//	t_image		*i3 = &s_game.map->texture[WALL].image[WEST];
-//	t_image		*i4 = &s_game.map->texture[WALL].image[EAST];
-//	t_image		*i5 = &s_game.map->texture[FLOOR].image[NORTH];
-//	t_image		*i6 = &s_game.map->texture[CEILING].image[NORTH];
-//	int			*width_f1 = &i1->width;
-//	int			*height_f1 = &i1->height;
-//	int			*width_f2 = &i2->width;
-//	int			*height_f2 = &i2->height;
-//	int			*width_f3 = &i3->width;
-//	int			*height_f3 = &i3->height;
-//	int			*width_f4 = &i4->width;
-//	int			*height_f4 = &i4->height;
-//	int			*width_f5 = &i5->width;
-//	int			*height_f5 = &i5->height;
-//	int			*width_f6 = &i6->width;
-//	int			*height_f6 = &i6->height;
-//
-//	i1->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f1, width_f1, height_f1);
-//	i2->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f2, width_f2, height_f2);
-//	i3->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f3, width_f3, height_f3);
-//	i4->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f4, width_f4, height_f4);
-//	i5->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f5, width_f5, height_f5);
-//	i6->mlx_img = mlx_xpm_file_to_image(s_game.graphx->mlx_ptr, (char *)f6, width_f6, height_f6);
-//	i1->data = mlx_get_data_addr(i1->mlx_img, &i1->bits_per_pixel, &i1->size_line, &i1->endian);
-//	i2->data = mlx_get_data_addr(i2->mlx_img, &i2->bits_per_pixel, &i2->size_line, &i2->endian);
-//	i3->data = mlx_get_data_addr(i3->mlx_img, &i3->bits_per_pixel, &i3->size_line, &i3->endian);
-//	i4->data = mlx_get_data_addr(i4->mlx_img, &i4->bits_per_pixel, &i4->size_line, &i4->endian);
-//	i5->data = mlx_get_data_addr(i5->mlx_img, &i5->bits_per_pixel, &i5->size_line, &i5->endian);
-//	i6->data = mlx_get_data_addr(i6->mlx_img, &i6->bits_per_pixel, &i6->size_line, &i6->endian);
-//	map->texture[FLOOR].rgb[0] = 0xffecbf;
-//	map->texture[CEILING].rgb[0] = 0x43629d;
-/* to remove */
-//ft_destroy_graphx(&s_game);
+t_mlx_exception	ft_open_texture(t_texture *texture, void *mlx_ptr)
+{
+	t_image			*tmp_img;
+	char			*tmp_path;
+	t_orientation	e_orientation;
+	t_element		e_element;
+
+	e_element = WALL;
+	while (e_element < N_ELEMENT)
+	{
+		e_orientation = NORTH;
+		while ((e_element < FLOOR || e_orientation == 0) \
+				&& e_orientation < N_ORIENTATION)
+		{
+			tmp_img = &texture[e_element].image[e_orientation];
+			tmp_path = texture[e_element].path[e_orientation];
+			if (ft_get_xpm(mlx_ptr, tmp_img, tmp_path) != 0)
+			{
+				ft_destroy_texture(mlx_ptr, texture, e_orientation, e_element);
+				return (CREATE_IMAGE_ERROR);
+			}
+			e_orientation++;
+		}
+		e_element++;
+	}
+	return (NO_EXCEPTION);
+}
+
+void	ft_destroy_texture(void *mlx_ptr, t_texture *texture, \
+							t_orientation e_orientation, t_element e_element)
+{
+	t_image			*tmp_img;
+	t_orientation	e_tmp_orientation;
+	t_element		e_tmp_element;
+
+	e_tmp_element = WALL;
+	while (e_tmp_element <= e_element && e_tmp_element < N_ELEMENT)
+	{
+		e_tmp_orientation = NORTH;
+		while (((e_tmp_element != e_element \
+				&& e_tmp_orientation < N_ORIENTATION) \
+				|| e_tmp_orientation < e_orientation)
+			   && (e_tmp_element < FLOOR || e_tmp_orientation == 0))
+		{
+			tmp_img = &texture[e_tmp_element].image[e_tmp_orientation];
+			if (tmp_img->mlx_img != NULL)
+				mlx_destroy_image(mlx_ptr, tmp_img->mlx_img);
+			e_tmp_orientation++;
+		}
+		e_tmp_element++;
+	}
+}
+
+static int	ft_get_xpm(void *mlx_ptr, t_image *img, char *path)
+{
+	if (path[0] != '\0')
+	{
+		img->mlx_img = mlx_xpm_file_to_image(mlx_ptr, path, &img->width, \
+											&img->height);
+		if (img->mlx_img == NULL)
+			return (-1);
+		ft_mlx_get_data_addr(img);
+	}
+	else
+	{
+		img->mlx_img = NULL;
+	}
+	return (0);
+}
